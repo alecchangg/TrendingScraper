2d68e05fa6c3fcf17ff96a9ae0f5a06556ca8e31
Alec Chang <amc8579@psu.edu>
2022-12-25 22:53:05 -0500

    formatting and documentation

--- load.py
+++ load.py
@@ -4,20 +4,25 @@ from datetime import date
 
 BASE = "http://127.0.0.1:5000/"
 
+#adds/updates the date dimension for today's date
 today = date.today().strftime("%Y-%m-%d")
 response = requests.get(BASE + "warehouse/date/", {'date': today})
 if response.json()['data'] == []:
     response = requests.put(BASE + "warehouse/date/", {'date': today})
+today_key = response.json()['data'][0][0]
 
+#get request to staging area for new data and converts JSON serializable object to pandas dataframes
 response = requests.get(BASE + "staging/out/")
 data = response.json()["data"]
-
 pd.set_option("display.max_columns", None)
 df = pd.DataFrame(data, columns = ['rank', 'name', 'views', 'likes', 'channel', 'subscribers'])
-
 channel_df = pd.DataFrame().assign(name = df['channel'], subscribers = df['subscribers'])
 
+
+#iterate through each row and adds/updates dimension and fact tables
 for index, row in df.iterrows():
+
+    #adds/updates channel info for current row
     response = requests.get(BASE + "warehouse/channel/", {'name': row['channel']})
     if response.json()['data'] == []:
         jsonData = {}
@@ -28,15 +33,3 @@ for index, row in df.iterrows():
         channel_key = response.json()['data'][0][0]
         subscribers = row['subscribers']
         response = requests.patch(BASE + "warehouse/channel/", {'key': channel_key, 'subscribers': subscribers})
-
-
-today = date.today().strftime("%Y-%m-%d")
-response = requests.get(BASE + "warehouse/date/", {'date': today})
-if response.json()['data'] == []:
-    response = requests.put(BASE + "warehouse/date/", {'date': today})
-
-print(channel_df)
-
-
-
-
